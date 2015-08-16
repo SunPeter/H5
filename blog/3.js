0ec285038476b45d3e102fa0c10c5a4e6915edc0
SunPeter <ssd3237649@gmail.com>
2015-08-17 00:34:22 +0800

    add

--- blog/3.js
+++ blog/3.js
@@ -7,18 +7,20 @@ seajs.use(["common"], function(common) {
     ctx = canvas.getContext("2d");
     var width = canvas.width;
     var height = canvas.height;
-    var bounce = -0.6;
+    var bounce = -0.7;
     var angle = 20;
-    var cos = Math.cos(angle/180*Math.PI),
-        sin = Math.sin(angle/180*Math.PI);
+    var cos = Math.cos(angle / 180 * Math.PI),
+        sin = Math.sin(angle / 180 * Math.PI);
     //中心点
     var centerX = width / 2,
         centerY = height / 2;
 
     //初始点
     var ball = new Ball(100, 50, 5);
-    var line = new Line(50,100,200,100);
-    line.rotation = angle/180*Math.PI;
+    var line = new Line(0, 0, 200, 0);
+    line.x = 50, line.y = 100;
+    line.rotation = angle / 180 * Math.PI;
+    var boundry = line.getBoundry();
     (function drawFrame() {
         requestAnimationFrame(drawFrame, canvas);
         ctx.clearRect(0, 0, width, height);
@@ -30,25 +32,27 @@ seajs.use(["common"], function(common) {
         var x1 = ball.x - line.x;
         var y1 = ball.y - line.y;
 
-        var x2 = x1*cos + y1*sin;
-        var y2 = y1*cos - x1*sin;
+        var x2 = x1 * cos + y1 * sin;
+        var y2 = y1 * cos - x1 * sin;
 
-        var vx2 = ball.vx*cos + ball.vy*sin;
-        var vy2 = ball.vy*cos - ball.vx*sin;
+        if (y2 + ball.radius > 0 && ball.x > boundry.x && ball.x <= boundry.x + boundry.width) {
+            var vx2 = ball.vx * cos + ball.vy * sin;
+            var vy2 = ball.vy * cos - ball.vx * sin;
 
-        if(y2 + ball.radius > 0){
             y2 = -ball.radius;
-            vy2 *= -bounce;
+            vy2 *= bounce;
+
+            x1 = x2 * cos - y2 * sin;
+            y1 = y2 * cos + x2 * sin;
+
+            ball.vx = vx2 * cos - vy2 * sin;
+            ball.vy = vy2 * cos + vx2 * sin;
+
+            ball.x = line.x + x1;
+            ball.y = line.y + y1;
         }
 
-        x1 = x2*cos - y2*sin;
-        y1 = y2*cos + x2*sin;
 
-        ball.vx = vx2*cos - vy2*sin;
-        ball.vy = vy2*cos + vx2*sin;
-
-        ball.x = line.x + x1;
-        ball.y = line.y + y1;
 
         ball.render(ctx);
         line.render(ctx);
--- blog/vendor/common.js
+++ blog/vendor/common.js
@@ -7,16 +7,33 @@ define("common", [], function(require, exports) {
         this.ax = 0;
         this.ay = 0;
         this.radius = r;
+        this.bounce = -1;
         this.render = function(ctx) {
-            ctx.save();
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            ctx.fill();
-            ctx.closePath();
-            ctx.restore();
-        }
+                ctx.save();
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                ctx.fill();
+                ctx.closePath();
+                ctx.restore();
+            },
+            this.move = function(w, h) {
+                if (this.x - this.radius <= 0) {
+                    this.vx *= this.bounce;
+                    this.x = this.radius;
+                } else if (this.x + this.radius >= w) {
+                    this.vx *= this.bounce;
+                    this.x = w - this.radius;
+                }
+                if (this.y - this.radius <= 0) {
+                    this.vy *= this.bounce;
+                    this.y = this.radius;
+                } else if (this.y + this.radius >= h) {
+                    this.vy *= this.bounce;
+                    this.y = h - this.radius;
+                }
+            }
     }
-    exports.Line = function(sx,sy,dx,dy){
+    exports.Line = function(sx, sy, dx, dy) {
         this.x = 0;
         this.y = 0;
         this.sx = sx;
@@ -24,28 +41,28 @@ define("common", [], function(require, exports) {
         this.dx = dx;
         this.dy = dy;
         this.rotation = 0;
-        this.render = function(ctx){
+        this.render = function(ctx) {
             ctx.save();
-            ctx.beginPath();
+            ctx.translate(this.x, this.y);
             ctx.rotate(this.rotation);
-            ctx.translate(this.x,this.y);
-            ctx.moveTo(sx,sy);
-            ctx.lineTo(dx,dy);
+            ctx.beginPath();
+            ctx.moveTo(sx, sy);
+            ctx.lineTo(dx, dy);
             ctx.stroke();
             ctx.restore();
         }
-        this.getBoundry = function(){
+        this.getBoundry = function() {
             var cos = Math.cos(this.rotation),
                 sin = Math.sin(this.rotation);
-            var sx = this.sx*cos+this.sy*sin,
-                sy = this.sy*cos-this.sx*sin;
-            var dx = this.dx*cos+this.dy*sin,
-                dy = this.dy*cos-this.dx*sin;
+            var sx = this.sx * cos + this.sy * sin,
+                sy = this.sy * cos - this.sx * sin;
+            var dx = this.dx * cos + this.dy * sin,
+                dy = this.dy * cos - this.dx * sin;
             return {
-                x:this.x + Math.min(sx,dx),
-                y:this.y + Math.min(sy,dy),
-                width:Math.max(sx,dx) - Math.min(sx,dx),
-                height:Math.max(sy,dy) - Math.min(sy,dy)
+                x: this.x + Math.min(sx, dx),
+                y: this.y + Math.min(sy, dy),
+                width: Math.max(sx, dx) - Math.min(sx, dx),
+                height: Math.max(sy, dy) - Math.min(sy, dy)
             }
         }
     }
