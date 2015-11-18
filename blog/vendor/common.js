f286d44b1bc46bcda15b4c40103b629b0eeaee98
sunshengda <sunshengda@meituan.com>
2015-11-18 11:52:24 +0800

    正向运动

--- blog/vendor/common.js
+++ blog/vendor/common.js
@@ -8,10 +8,12 @@ define("common", [], function(require, exports) {
         this.ay = 0;
         this.radius = r;
         this.bounce = -1;
+        this.bg = '#000';
         this.render = function(ctx) {
                 ctx.save();
                 ctx.beginPath();
                 ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                ctx.fillStyle = this.bg;
                 ctx.fill();
                 ctx.closePath();
                 ctx.restore();
@@ -30,50 +32,6 @@ define("common", [], function(require, exports) {
                 } else if (this.y + this.radius >= h) {
                     this.vy *= this.bounce;
                     this.y = h - this.radius;
-                }
-            },
-            this.collision = function(ball1, ball2) {
-                var dx = ball2.x - ball1.x,
-                    dy = ball2.y - ball1.y;
-                var dist = Math.sqrt(dx * dx + dy * dy);
-
-                if (dist < ball1.radius + ball2.radius) {
-
-                    var angle = Math.atan2(dy, dx);
-                    var cos = Math.cos(angle),
-                        sin = Math.sin(angle);
-                    //rotate 
-                    var x1 = 0,
-                        y1 = 0;
-                    var x2 = dx * cos + dy * sin,
-                        y2 = dy * cos - dx * sin;
-
-                    var vx1 = ball1.vx * cos + ball1.vy * sin,
-                        vy1 = ball1.vy * cos - ball1.vx * sin;
-                    var vx2 = ball2.vx * cos + ball2.vy * sin,
-                        vy2 = ball2.vy * cos - ball2.vx * sin;
-
-                    var relativeV = vx1 - vx2;
-                    vx1 = ((ball1.mass - ball2.mass) * vx1 + 2 * ball2.mass * vx2) / (ball1.mass + ball2.mass);
-                    vx2 = vx1 + relativeV;
-
-                    x1 += vx1;
-                    x2 += vx2;
-                    //rotate positions back
-                    var x1Final = x1 * cos - y1 * sin,
-                        y1Final = y1 * cos + x1 * sin,
-                        x2Final = x2 * cos - y2 * sin,
-                        y2Final = y2 * cos + x2 * sin;
-                    //adjust positions to actual screen positions
-                    ball2.x = ball1.x + x2Final;
-                    ball2.y = ball1.y + y2Final;
-                    ball1.x = ball1.x + x1Final;
-                    ball1.y = ball1.y + y1Final;
-                    //rotate velocities back
-                    ball1.vx = vx1 * cos - vy1 * sin;
-                    ball1.vy = vy1 * cos + vx1 * sin;
-                    ball2.vx = vx2 * cos - vy2 * sin;
-                    ball2.vy = vy2 * cos + vx2 * sin;
                 }
             }
     }
@@ -109,5 +67,59 @@ define("common", [], function(require, exports) {
                 height: Math.max(sy, dy) - Math.min(sy, dy)
             }
         }
+    }
+    exports.Segment = function(x,y,width,height,rotation){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.scaleX = 1;
+        this.scaleY = 1;
+        this.rotation = rotation/180 * Math.PI;
+        this.strokeStyle = "#000";
+        this.lineWidth = 1;
+        this.draw = function(){
+          /**
+           *  c 为半径  d为实际宽度
+           */
+            var w = this.width,h = this.height;
+            var c = h/2, d = w + h;
+            ctx.save();
+            ctx.translate(this.x,this.y);
+            ctx.rotate(this.rotation);
+            ctx.scale(this.scaleX, this.scaleY);
+            ctx.beginPath();
+            ctx.moveTo(0 , -c);
+            ctx.lineTo(w, -c);
+            ctx.quadraticCurveTo( w + c, -c , w + c , 0);
+            ctx.quadraticCurveTo( w + c, c , w , c);
+            ctx.lineTo(0, c);
+            ctx.quadraticCurveTo( -c, c , -c , 0);
+            ctx.quadraticCurveTo( -c, -c , 0 , -c);
+            ctx.closePath();
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(0,0,2,0,Math.PI*2,true);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(w,0,2,0,Math.PI*2,true);
+            ctx.stroke();
+            ctx.restore();
+
+
+        },
+        this.render = function(ctx){
+          this.draw();
+        },
+        this.setRotation = function  (rotation) {
+          this.rotation = rotation/180 * Math.PI;
+        },
+        this.getPin = function () {
+          return {
+            x: this.x + Math.cos(this.rotation) * this.width,
+            y: this.y + Math.sin(this.rotation) * this.width
+          };
+        };
     }
 })
